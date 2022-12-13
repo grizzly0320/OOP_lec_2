56f2fb1f3e8191bb8a9296c58d0b05cc0915d465
grizzly0320 <91096782+grizzly0320@users.noreply.github.com>
2022-12-13 21:55:51 +0300

    Добавьте файлы проекта.

---
+++ OOP_lec_2/Complex.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_lec_2 
+{
+
+    public class TrigComplex : Complex
+    {
+        private double r, phi;
+        public TrigComplex(double r, double phi)
+            : base(r * Math.Cos(phi), r * Math.Sin(phi))
+        {
+            this.r = r;
+            this.phi = phi;
+        }
+        public TrigComplex(Complex z)
+            : base(z)
+        {
+            phi = Math.Atan(Im / Re);
+            if (re < 0)
+            {
+                phi += im >= 0 ? Math.PI : -Math.PI;
+            }
+            r = Math.Sqrt(Re * Re + Im * Im);
+        }
+        public static TrigComplex operator +(TrigComplex first, TrigComplex second)
+        {
+            return new TrigComplex((first as Complex) + second);
+        }
+        public override string ToString()
+        {
+            return r + "(Cos(" + phi + ") + i Sin(" + phi + ")";
+        }
+    }
+    public class Complex
+    {
+        protected static int count = 0;
+        protected int index;
+        public static int Count
+        {
+            get => count;
+
+        }
+        public double? this[char part]
+        {
+            get
+            {
+                if (part == 'r' || part == 'R') return Re;
+                if (part == 'i' || part == 'I') return Im;
+                return null;
+            }
+            set
+            {
+                if (part == 'r' || part == 'R') Re = value ?? 0;
+                if (part == 'i' || part == 'I') Im = value ?? 0;
+            }
+        }
+        public Complex()
+        {
+            count++;
+            index = count;
+        }
+        protected double re, im;
+        //Конструктор по умолчанию
+        public Complex(double re, double im)
+        {
+            this.re = re;
+            this.im = im;
+            count++;
+            index = count;
+        }
+        //Конструктор копирования
+        public Complex(Complex other)
+        {
+            Re = other.Re;
+            Im = other.Im;
+            count++;
+            index = count;
+        }
+        /*Свойство - это типо элементов класса, который используется для получения и задания значений полей класса,
+        либо для получения величин вычисляемых на основе класса; занимаю промежуточное значение между полями и методами*/
+        //Автореализуемое свойство - совмещает свойства поля и метода 
+        public double Re {
+            get => re;
+            set => re = value;
+        }
+
+        public double Im {
+            get => im;
+            set => im = value;
+        }
+        public double roIM => im; //readonly im
+        public double Abs => Math.Sqrt(Re * Re + Im * Im);
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(index);
+            sb.Append("):");
+            if (Math.Abs(Re) > 10 * double.Epsilon || Math.Abs(Im) < 10 * double.Epsilon)
+                sb.Append(Re);
+            if (Math.Abs(Im) > 10 * double.Epsilon)
+            {
+                sb.Append(Im > 0 ? "+" : "-");
+                if (Math.Abs(Math.Abs(Im) - 1) > 10 * double.Epsilon)
+                {
+                    sb.Append(Math.Abs(Im));
+                }
+                sb.Append("i");
+            }
+            return sb.ToString();
+        }
+        public Complex Plus(Complex other) => new Complex(this.Re + other.Re, Im + other.Im);
+        public static Complex operator +(Complex z1, Complex z2)
+        {
+            return new Complex(z1.Re + z2.Re, z1.Im + z2.Im);
+        }
+        public static Complex operator -(Complex z1, Complex z2) => new Complex(z1.Re - z2.Re, z1.Im - z2.Im);
+        public static Complex operator -(Complex z1) =>
+            new(z1.Re, -z1.Im);
+        public static bool operator ==(Complex it, Complex other)
+        {
+            return Math.Abs(it.Re - other.Re) < 1e-25 &&
+                Math.Abs(it.Im - other.Im) < 1e-25;
+        }
+        public override bool Equals(object? obj)
+        {
+            //if (obj == null) return false;
+            //if (obj.GetType() != typeof(Complex)) return false;
+            //Complex x = (Complex)obj;
+            if (obj is Complex x)
+                return Math.Abs(Re - x.Re) < 1e-25 &&
+                    Math.Abs(Im - x.Im) < 1e-25;
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Re, Im);
+        }
+        public static bool operator !=(Complex it, Complex other) => !(it == other);
+        // Операторы, окторые можно перегрузить
+        // Унарные операторы:
+        // +, -, !, ~, ++, --
+        // Бинарные операторы:
+        // +, -, *, /, %
+        // Операторы сравнения:
+        // ==, !=, <, >, <=, >=
+        // Поразрядные операторы:
+        // &, |, ^, <<, >>
+        // Логические операторы:
+        // &&, ||
+    }
+}
---
+++ OOP_lec_2/Program.cs
@@ -0,0 +1,95 @@
+﻿using static System.Console;
+using OOP_lec_2;
+
+TrigComplex tz1 = new TrigComplex(1, 0.5);
+TrigComplex tz2 = new TrigComplex(1, -1.5);
+var tz3 = tz1 + tz2;
+WriteLine(tz3);
+WriteLine(new Complex(tz3));
+
+//Complex z1 = new Complex();
+//WriteLine(Complex.Count);
+//Complex z2 = new Complex(3, -5);
+//WriteLine(Complex.Count);
+//Complex z3 = new Complex(z2);
+//Complex z4 = z2;
+//z2.Im = -1;
+//WriteLine(z1.Abs);
+//WriteLine(z2.Abs);
+//WriteLine(z3.Abs);
+//WriteLine(z4.Abs);
+//WriteLine(z1);
+//WriteLine(z2);
+//WriteLine(z3);
+//WriteLine(z4);
+//Complex z5 = z2.Plus(z3);
+//WriteLine(z5);
+//Complex z6 = z3 + z2;
+//WriteLine(z6);
+//WriteLine(-z6);
+//z6 += z4;
+//WriteLine(z6);
+
+//WriteLine();
+//int? i1 = null;
+//if (i1 != null)
+//{
+//    int i11 = (int)i1;
+//    int i2 = i11 + 1;
+//}
+//if (i1 is { } i12)
+//{
+//    int i2 = i12 + 1;
+//}
+//var i3 = 1 + i1 ?? 0;
+//Complex? zz = null;
+//WriteLine(zz?.Re.ToString() ?? "null");
+//int xx = default;
+//WriteLine(xx);
+//int[] mas = new int[3];
+
+//MyClass mc = new();
+//mc.AddData(10);
+//mc.AddData(2, 4, 6, 8, 10);
+//(int a, int b, int c) = mc.GetElems();
+
+//Person p = new("Иван", 23);
+//(string name, int age) = p;
+
+//class MyClass
+//{
+//    private List<int> data = new();
+//    public void AddData(int elem)
+//    {
+//        data.Add(elem);
+//    }
+//    public void AddData(List<int> elems)
+//    {
+//        data.AddRange(elems);
+//    }
+
+//    public void AddData(params int[] elems)
+//    {
+//        data.AddRange(elems);
+//    }
+//    public int[]  GetElems()
+//    {
+//        return new[] { data[0], data[1], data[2] }; 
+//    }
+//} 
+
+//class Person{
+//    private string name;
+//    private int age;
+//    public Person(string name, int age)
+//    {
+//        this.name = name;
+//        this.age = age;
+//    }
+
+//    public void Deconstruct(out string pName, out int pAge)
+//    {
+//        pName = name;
+//        pAge = age;
+//    }
+//}
